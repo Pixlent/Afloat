a72d04d51fd2714099bf43aacc4f1124c9b0727c
Pixlent <2beheima@gmail.com>
2023-11-05 12:59:05 +0100

    Added some debug commands

--- server/src/main/java/me/pixlent/Main.java
+++ server/src/main/java/me/pixlent/Main.java
@@ -1,43 +1,43 @@
 package me.pixlent;
 
-import me.pixlent.phasemachine.LevelFactory;
+import me.pixlent.commands.GamemodeCommand;
+import me.pixlent.commands.TeleportCommand;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.GameMode;
 import net.minestom.server.event.player.PlayerLoginEvent;
-import net.minestom.server.extras.MojangAuth;
-import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+         * Initialize the minecraft server
+         */
         final var server = MinecraftServer.init();
 
-        MojangAuth.init();
-        server.start("0.0.0.0", 25565);
+        registerCommands();
 
-        final var level = new LevelFactory()
-                .addPhase(List.of(new StartupService()))
-                .addPhase(List.of(new WaitingService()))
-                .addPhase(List.of(new CleanupService()))
-                .build();
+        final var instance = MinecraftServer.getInstanceManager().createInstanceContainer();
 
-        level.setGenerator(unit ->
-                unit.modifier().fillHeight(0, 40, Block.STONE));
-        level.setChunkSupplier(LightingChunk::new);
-
-        MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, event -> {
-            final var player = event.getPlayer();
-
-            player.setPermissionLevel(4);
-            player.setGameMode(GameMode.CREATIVE);
-            player.setRespawnPoint(new Pos(0, 40, 0));
-
-            event.setSpawningInstance(level);
+        instance.setGenerator(unit -> {
+            unit.modifier().fillHeight(0, 10, Block.STONE);
         });
 
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, event -> {
+            event.setSpawningInstance(instance);
+            event.getPlayer().setRespawnPoint(new Pos(0, 10, 0));
+        });
+
+        /*
+         * Start and hook the minecraft server
+         */
+        server.start("0.0.0.0", 25565);
         AfloatServer.hook(server);
+    }
+
+    public static void registerCommands() {
+        final var commandManager = MinecraftServer.getCommandManager();
+
+        commandManager.register(new GamemodeCommand());
+        commandManager.register(new TeleportCommand());
     }
 }
